0309b2d149fd66339919d8bd4f46e34fd9812c99
tommywelleby <tommywelleby@gmail.com>
2013-04-26 06:42:41 +0000

    Added CaseWindow.

--- WpfApplication2/CaseWindow.xaml.cs
+++ WpfApplication2/CaseWindow.xaml.cs
@@ -10,24 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WpfApplication2
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for CaseWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class CaseWindow : Window
     {
-        public MainWindow()
+        public CaseWindow()
         {
             InitializeComponent();
-        }
-
-        public void OpenCaseWindow(int caseNumber)
-        {
-        
         }
     }
 }
