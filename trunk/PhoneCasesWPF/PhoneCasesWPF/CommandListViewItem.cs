cf5609761c434bb3f0b42ce24a6ad44fad6951ba
tommywelleby@gmail.com <tommywelleby@gmail.com@49f16bb8-e0f2-3bdf-37dc-455a5dfc798e>
2013-05-02 14:10:11 +0000

    Added experimantal CommandListViewItem.

---
+++ trunk/PhoneCasesWPF/PhoneCasesWPF/CommandListViewItem.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace PhoneCasesWPF
+{
+
+    public class CommandListViewItem : ListViewItem
+    {
+        public CommandListViewItem():base() {
+            //DefaultStyleKeyProperty.OverrideMetadata(typeof(CommandListViewItem),
+            //new FrameworkPropertyMetadata(typeof(ListViewItem)));
+
+            this.MouseDoubleClick += CommandListViewItem_MouseDoubleClick;
+
+        }
+
+        private void CommandListViewItem_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            
+            if(Command!=null)
+                Command.Execute(((CommandListViewItem)e.Source).Parameter);
+        }
+
+        public static DependencyProperty CommandProperty
+        = DependencyProperty.Register(
+            "Command",
+            typeof(ICommand),
+            typeof(CommandListViewItem));
+
+        public static DependencyProperty ParameterProperty
+        = DependencyProperty.Register(
+            "Parameter",
+            typeof(object),
+            typeof(CommandListViewItem));
+
+        public ICommand Command
+        {
+            get
+            {
+                return (ICommand)GetValue(CommandProperty);
+            }
+
+            set
+            {
+                SetValue(CommandProperty, value);
+            }
+        }
+        public object Parameter
+        {
+            get
+            {
+                return GetValue(ParameterProperty);
+            }
+
+            set
+            {
+                SetValue(ParameterProperty, value);
+            }
+        }
+
+
+        
+    }
+}
