b841bde0236efd3f0b7999c6fdbee41128fd901b
tommywelleby@gmail.com <tommywelleby@gmail.com@49f16bb8-e0f2-3bdf-37dc-455a5dfc798e>
2013-05-06 16:01:43 +0000

    Implemented Transmitter

--- trunk/PhoneCasesWPF/PhoneCasesServer/Client.cs
+++ trunk/PhoneCasesWPF/PhoneCasesServer/Client.cs
@@ -10,6 +10,9 @@ namespace PhoneCases.Server
     {
         private string m_ip;
         private string m_port;
+
+        public string IP { get { return m_ip; } set { m_ip = value; } }
+        public string Port { get { return m_port; } set { m_port = value; } }
         
         public Client(string ip, string port)
         {
