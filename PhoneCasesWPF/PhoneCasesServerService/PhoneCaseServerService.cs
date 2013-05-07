48ef031403ff807a0e3c1c784fa3747491678ee8
tommywelleby <tommywelleby@gmail.com>
2013-05-07 06:41:51 +0000

    Added PhoneCasesServerService project

---
+++ PhoneCasesWPF/PhoneCasesServerService/PhoneCaseServerService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PhoneCases.Server;
+
+namespace PhoneCases.Server.Service
+{
+    class PhoneCaseServerService
+    {
+
+        static void Main(string[] args)
+        {
+            Transmitter t = new Transmitter();
+        }
+    }
+}
