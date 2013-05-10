049244d375fb108bb8d8425c584580d539e6009b
tommywelleby@gmail.com <tommywelleby@gmail.com@49f16bb8-e0f2-3bdf-37dc-455a5dfc798e>
2013-05-10 12:43:31 +0000

    Added timeformatting

--- trunk/PhoneCasesWPF/PhoneCasesWPF/MainWindow.xaml.cs
+++ trunk/PhoneCasesWPF/PhoneCasesWPF/MainWindow.xaml.cs
@@ -440,5 +440,29 @@ namespace PhoneCases.WPFGUI
         }  
 
     }
+
+    [ValueConversion(typeof(object), typeof(string))]
+    public class SecondsToTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType,
+            object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value != null)
+            {
+                string valueString = value.ToString();
+                int seconds = int.Parse(valueString);
+                TimeSpan time = TimeSpan.FromSeconds(seconds);
+
+                return string.Format("{0:D2}:{1:D2}:{2:D2}",time.Hours,time.Minutes,time.Seconds);
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType,
+            object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+    }
    
 }
