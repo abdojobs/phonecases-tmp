5dcf8e99728877372da98971f4aaefa7cd68a1c3
tommywelleby <tommywelleby@gmail.com>
2013-05-10 14:45:33 +0000

    Added "okänd" to basedataquery for upcoming use of unkown/not-yet-added numbers.

--- PhoneCasesDB/InsertBaseData.sql
+++ PhoneCasesDB/InsertBaseData.sql
@@ -21,26 +21,32 @@ BEGIN
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
 		SET IDENTITY_INSERT pc.Users ON
+	insert into PhoneCases.pc.Users(Id,Name,PhoneNumber) values (0,'Alla','0');
 	insert into PhoneCases.pc.Users(Id,Name,PhoneNumber) values (1,'Tommy','358967047716681');
 	insert into PhoneCases.pc.Users(Id,Name,PhoneNumber) values (2,'Pelle','0734186414');
 	insert into PhoneCases.pc.Users(Id,Name,PhoneNumber) values (3,'Anders','0734186409');
+	insert into PhoneCases.pc.Users(Id,Name,PhoneNumber) values (4,'Emulator','000000000000000');
 	SET IDENTITY_INSERT pc.Users OFF
 	
 	SET IDENTITY_INSERT pc.Locations ON
+	insert into PhoneCases.pc.Locations(Id,Name) values (0,'Okänd');
 	insert into PhoneCases.pc.Locations(Id,Name) values (1,'Värmland');
 	SET IDENTITY_INSERT pc.Locations OFF
 
 	SET IDENTITY_INSERT pc.CompanyTypes ON
+	insert into PhoneCases.pc.CompanyTypes(Id,Name) values (0,'Okänd');
 	insert into PhoneCases.pc.CompanyTypes(Id,Name) values (1,'Amb');
 	insert into PhoneCases.pc.CompanyTypes(Id,Name) values (2,'Rtj');
 	insert into PhoneCases.pc.CompanyTypes(Id,Name) values (3,'Polis');
 	SET IDENTITY_INSERT pc.CompanyTypes OFF
 
 	SET IDENTITY_INSERT pc.Companies ON
+	insert into PhoneCases.pc.Companies(Id,Name,CompanyTypesId,LocationId) values (0,'Okänd',0,0);
 	insert into PhoneCases.pc.Companies(Id,Name,CompanyTypesId,LocationId) values (1,'Landstinget i Värmland',1,1);
 	SET IDENTITY_INSERT pc.Companies OFF
 
 	SET IDENTITY_INSERT pc.Customers ON
+	insert into PhoneCases.pc.Customers(Id,Name,CompanyId,PhoneNumber) values (0,'Okänd',1,'0');
 	insert into PhoneCases.pc.Customers(Id,Name,CompanyId,PhoneNumber) values (1,'Emil Palm',1,'0');
 	insert into PhoneCases.pc.Customers(Id,Name,CompanyId,PhoneNumber) values (2,'Tommy Welleby',1,'0707269686');
 	insert into PhoneCases.pc.Customers(Id,Name,CompanyId,PhoneNumber) values (3,'Emulator',1,'15555215554');
