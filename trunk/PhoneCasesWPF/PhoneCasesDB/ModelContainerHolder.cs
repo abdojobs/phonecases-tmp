be6ec0fa242d777e543f8ea81047f3076890695d
tommywelleby@gmail.com <tommywelleby@gmail.com@49f16bb8-e0f2-3bdf-37dc-455a5dfc798e>
2013-05-13 10:47:45 +0000

    BUG 9: Added functionality to receive unknown numbers.

--- trunk/PhoneCasesWPF/PhoneCasesDB/ModelContainerHolder.cs
+++ trunk/PhoneCasesWPF/PhoneCasesDB/ModelContainerHolder.cs
@@ -35,8 +35,10 @@ namespace PhoneCases.DB
                 Users owner = Model.Users.Find(ownerId);
                 if (customer == null)
                 {
-                    //Use "Okänd kund" and make the user choose from existing customers or create a new.
-                    throw new Exception("Customer not found"); //Should notify pcuser and create new customer
+                    //Make new "Okänd kund" with customerNumber and make it possible to change user if unkown. also add feature to unlock and edit customer on case.
+                    //throw new Exception("Customer not found"); //Should notify pcuser and create new customer
+                    int customerId = NewCustomer(customerNumber);
+                    customer = Model.Customers.Find(customerId);
                 }
                     
                 if (owner != null)
@@ -68,6 +70,21 @@ namespace PhoneCases.DB
 
             return -1;
             
+        }
+        public static int NewCustomer(string CustomerNumber, string CustomerName = "Okänd", int CompanyId = 0)
+        {
+            try
+            {
+                Companies company = Model.Companies.Find(CompanyId);
+                Customers entity = Model.Customers.Add(new Customers() { PhoneNumber = CustomerNumber, Company=company, Name = CustomerName });
+                Model.SaveChanges();
+                return entity.Id;
+            }
+            catch (System.Exception e)
+            {
+            	//Error.
+            }
+            return -1;
         }
     }
 }
